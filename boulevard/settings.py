0088c6b5972e8735824f644ff0439222e6e9b89d
Neal <33704976+nealmick@users.noreply.github.com>
2023-05-11 15:39:15 -0400

    Add files via upload

--- boulevard/settings.py
+++ boulevard/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -108,7 +109,7 @@ DEFAULT_RENDERER_CLASSES = [
     ]
 
 DEFAULT_AUTHENTICATION_CLASSES = [
-    'rest_framework.authentication.SessionAuthentication'
+    'rest_framework.authentication.TokenAuthentication',
 ]
 if DEBUG:
     DEFAULT_RENDERER_CLASSES += [
