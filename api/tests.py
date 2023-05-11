a5db564fd0036d08e7bfc24d11276a281c0ac1c2
Neal <33704976+nealmick@users.noreply.github.com>
2023-05-11 15:38:59 -0400

    Add files via upload

--- api/tests.py
+++ api/tests.py
@@ -1,38 +1,54 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from rest_framework import status
 import json
 from .models import Article
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 class ArticleTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
-        
-    #create article
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+    #authenticate test client with token
+    def get_authenticated_client(self):
+        self.client.force_authenticate(user=self.user, token=self.token)
+        return self.client
+
+    # create article
     def test_create_article(self):
         url = reverse('articles')
         data = {
             "title": "test",
             "content": "test"
         }
-        response = self.client.post(url, data=json.dumps(data), content_type='application/json')
+
+        response = self.get_authenticated_client().post(
+            url,
+            data=json.dumps(data),
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Token ' + self.token.key
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Article.objects.count(), 1)
-    #get articles list
+    
+    # get articles list
     def test_get_list_articles(self):
         url = reverse('articles')
-        response = self.client.get(url)
+        response = self.get_authenticated_client().get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    #get article detail
+    # get article detail
     def test_get_article_detail(self):
         article = Article.objects.create(title='Test Article', content='Test Content')
         url = reverse('article-detail', args=[article.pk])
-        response = self.client.get(url)
+        response = self.get_authenticated_client().get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Test Article')
 
-    #update article
+    # update article
     def test_update_article(self):
         article = Article.objects.create(title='Test Article', content='Test Content')
         url = reverse('article-detail', args=[article.pk])
@@ -40,14 +56,19 @@ class ArticleTestCase(TestCase):
             "title": "Updated Article",
             "content": "Updated Content"
         }
-        response = self.client.put(url, data=json.dumps(data), content_type='application/json')
+        response = self.get_authenticated_client().put(
+            url,
+            data=json.dumps(data),
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Token ' + self.token.key
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Updated Article')
 
-    #delete article
+    # delete article
     def test_delete_article(self):
         article = Article.objects.create(title='Test Article', content='Test Content')
         url = reverse('article-detail', args=[article.pk])
-        response = self.client.delete(url)
+        response = self.get_authenticated_client().delete(url, HTTP_AUTHORIZATION='Token ' + self.token.key)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(Article.objects.count(), 0)
--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('articles/', views.articles, name='articles'),
